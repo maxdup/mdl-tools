ff6adab86d23b4a7dfe5ba45a9f6808308ddde97
maxdup <mdupuis@hotmail.ca>
2019-09-21 21:53:28 -0400

    parse skin table and hitboxes. Added types

--- mdltools/parser.py
+++ mdltools/parser.py
@@ -17,32 +17,35 @@ def readcstr(f):
     return ''.join(iter(lambda: f.read(1).decode('ascii'), '\x00'))
 
 
+Vector = Struct('x' / Float32l, 'y' / Float32l, 'z' / Float32l)
+RadianEuler = Struct('x' / Float32l, 'y' / Float32l, 'z' / Float32l)
+Quaternion = Struct('x' / Float32l, 'y' / Float32l,
+                    'z' / Float32l, 'w' / Float32l)
+matrix3x4_t = Sequence(Float32l[4], Float32l[4], Float32l[4])
+
+
 mstudiobbox_t = Struct(
     'bone' / Int32sl,
     'group' / Int32sl,
-    'bbmin' / Float32l[3],
-    'bbmax' / Float32l[3],
+    'bbmin' / Vector,
+    'bbmax' / Vector,
     'szhitboxnameindex' / Int32sl,
     'unused' / Int32sl[8],
+    # todo: parse 'name'
 )
 
 mstudiohitboxset_t = Struct(
+    'i' / Index,
+
     'sznameindex' / Int32sl,
     'numhitboxes' / Int32sl,
     'hitboxindex' / Int32sl,
 
-    # 'hitboxes' / Pointer(this.hitboxindex,
-    #                     mstudiobbox_t[this.numhitboxes]),
+    'hitboxes' / Pointer(this.hitboxindex + this._.hitboxsetindex +
+                         (this.i*12), mstudiobbox_t[this.numhitboxes]),
+    'name' / Pointer(this.sznameindex + this._.hitboxsetindex +
+                     (this.i*12), CString('ascii'))
 )
-'''
-mstudiovertex_t = Struct(
-    mstudioboneweight_t m_BoneWeights;
-    Vector              m_vecPosition;
-    Vector              m_vecNormal;
-    Vector2D            m_vecTexCoord;
-    mstudiovertex_t() {}
-)
-'''
 
 mstudiocdtexture_t = Struct(
     'nameoffset' / Int32sl,
@@ -64,21 +67,93 @@ mstudiotexture_t = Struct(
                      (this.i*64), CString('ascii'))
 )
 
-mstudiomodel_t = Struct(
+'''
+mstudiovertex_t = Struct(
+    mstudioboneweight_t m_BoneWeights;
+    Vector              m_vecPosition;
+    Vector              m_vecNormal;
+    Vector2D            m_vecTexCoord;
+    mstudiovertex_t() {}
+)
+'''
+mstudiomesh_t = Struct(
+    'material' / Int32sl,
+    'modelindex' / Int32sl,
+    'numvertices' / Int32sl,
+    'vertexoffset' / Int32sl,
+    'numflexes' / Int32sl,
+    'flexindex' / Int32sl,
+    'materialtype' / Int32sl,
+    'materialparam' / Int32sl,
+    'meshid' / Int32sl,
+    'center' / Float32l[3],
+    'numLODVertexes' / Int32sl,  # ???
+    'unused' / Int32sl[8],
+)
 
+mstudioeyeball_t = Struct(
+    'sznameindex' / Int32sl,
+    'bone' / Int32sl,
+    'org' / Vector,
+    'zoffset' / Float32l,
+    'radius' / Float32l,
+    'up' / Vector,
+    'forward' / Vector,
+    'texture' / Int32sl,
+    'unused1' / Int32sl,
+    'iris_scale' / Float32l,
+    'unused2' / Int32sl,
+    'upperflexdesc' / Int32sl[3],
+    'lowerflexdesc' / Int32sl[3],
+    'uppertarget' / Float32l[3],
+    'lowertarget' / Float32l[3],
+    'upperlidflexdesc' / Int32sl,
+    'lowerlidflexdesc' / Int32sl,
+    'unused' / Int32sl[4],
+    'm_bNonFACS' / Flag,
+    'unused3' / Int32sl[3],  # actually char?
+    'unused4' / Int32sl[7],
+)
+
+mstudiomodel_t = Struct(
+    'i' / Index,
+    'name' / PaddedString(64, "ascii"),
+    'type' / Int32sl,
+    'boundingradius' / Float32l,
+    # mstudiomesh_t
+    'nummeshes' / Int32sl,
+    'meshindex' / Int32sl,
+    'numvertices' / Int32sl,
+    'vertexindex' / Int32sl,
+    'tangentsindex' / Int32sl,
+    'numattachments' / Int32sl,
+    'attachmentindex' / Int32sl,
+    'numeyeballs' / Int32sl,
+    'eyeballindex' / Int32sl,
+    'unused' / Int32sl[10],
+
+    # 'meshes' / Pointer(this.meshindex + this._.modelindex + this._._.bodypartindex +
+    #                   (this.i*148), mstudiomesh_t[this.nummeshes]),
+    # 'eyeballs' Pointer(mstudioeyeball_t)
 )
 
 mstudiobodyparts_t = Struct(
+    'i' / Index,
+
     'sznameindex' / Int32sl,
     'nummodels' / Int32sl,
     'base' / Int32sl,
     'modelindex' / Int32sl,
-    'name' / Pointer(this.sznameindex + this._.bodypartindex, CString('ascii'))
+
+    'name' / Pointer(this.sznameindex + this._.bodypartindex +
+                     (this.i*16), CString('ascii')),
+    'models' / Pointer(this.modelindex + this._.bodypartindex +
+                       (this.i*16), mstudiomodel_t[this.nummodels]),
 )
 
 mstudiomouth_t = Struct(
     'bone' / Int32sl,
-    'forward' / Float32l[3],
+    'forward' / Vector,
     'flexdesc' / Int32sl,
 )
 
@@ -96,8 +171,8 @@ mstudiomovement_t = Struct(
     'v0' / Float32l,
     'v1' / Float32l,
     'angle' / Float32l,
-    'vector' / Float32l[3],
-    'position' / Float32l[3],
+    'vector' / Vector,
+    'position' / Vector,
 )
 
 mstudioseqdesc_t = Struct(
@@ -109,8 +184,8 @@ mstudioseqdesc_t = Struct(
     'actweight' / Int32sl,
     'numevents' / Int32sl,
     'eventindex' / Int32sl,
-    'bbmin' / Float32l[3],
-    'bbmax' / Float32l[3],
+    'bbmin' / Vector,
+    'bbmax' / Vector,
     'numblends' / Int32sl,
     'animindexindex' / Int32sl,
     'movementindex' / Int32sl,
@@ -146,16 +221,22 @@ mstudiobone_t = Struct(
     'sznameindex' / Int32sl,
     'parent' / Int32sl,
     'bonecontroller' / Int32sl[6],
-    'pos' / Float32l[3],
-    'quat' / Float32l[4],
-    'rot' / Float32l[3],
-    'posscale' / Float32l[3],
-    'rotscale' / Float32l[3],
-    'poseToBone' / Float32l[12],
-    'qAlignment' / Float32l[4],
+
+    'pos' / Vector,
+    'quat' / Quaternion,
+    'rot' / RadianEuler,
+
+    'posscale' / Vector,
+    'rotscale' / Vector,
+
+    'poseToBone' / matrix3x4_t,
+    'qAlignment' / Quaternion,
+
     'flags' / Int32sl,
     'proctype' / Int32sl,
     'procindex' / Int32sl,
+    'physicsbone' / Int32sl,
+
     'surfacepropidx' / Int32sl,
     'contents' / Int32sl,
     'unused' / Int32sl[8],
@@ -217,8 +298,8 @@ mstudioflexrule_t = Struct(
 mstudiobbox_t = Struct(
     'bone' / Int32sl,
     'group' / Int32sl,
-    'bbmin' / Float32l[3],
-    'bbmax' / Float32l[3],
+    'bbmin' / Vector,
+    'bbmax' / Vector,
     'szhitboxnameindex' / Int32sl,
     'unused' / Int32sl[8],
 )
@@ -227,7 +308,7 @@ mstudioevent_t = Struct(
     'cycle' / Float32l,
     'event' / Int32sl,
     'type' / Int32sl,
-    'options[64]' / PaddedString(64, "ascii"),
+    'options' / PaddedString(64, "ascii"),
     'szeventindex' / Int32sl,
 )
 
@@ -235,7 +316,7 @@ mstudioattachment_t = Struct(
     'sznameindex' / Int32sl,
     'flags' / Int32ul,
     'localbone' / Int32sl,
-    'local' / Float32l[12],
+    'local' / matrix3x4_t,
     'unused' / Int32sl[8],
 )
 mstudioikchain_t = Struct(
@@ -262,8 +343,8 @@ mstudioikrule_t = Struct(
     'height' / Float32l,
     'radius' / Float32l,
     'floor' / Float32l,
-    'pos' / Float32l[3],
-    'q' / Float32l[4],
+    'pos' / Vector,
+    'q' / Quaternion,
     'compressedikerrorindex' / Int32sl,
     'unused2' / Int32sl,
     'iStart' / Int32sl,
@@ -294,7 +375,16 @@ mstudiolocalhierarchy_t = Struct(
     'localanimindex' / Int32sl,
     'unused' / Int32sl[4],
 )
-
+mstudioanim_t = Struct(
+    'bone' / Byte,
+    'flags' / Byte,
+    'nextoffset' / Int16sl,
+)
+mstudiovertanim_t = Struct(
+    'index' / Int16ul,
+    'speed' / Byte,
+    'side' / Byte,
+)
 mstudioanimblock_t = Struct(
     'datastart' / Int32sl,
     'dataend' / Int32sl,
@@ -305,27 +395,42 @@ mstudioanimsections_t = Struct(
 )
 
 mstudioanimdesc_t = Struct(
+    'i' / Index,
+
     'baseptr' / Int32sl,
     'sznameindex' / Int32sl,
     'fps' / Float32l,
     'flags' / Int32sl,
     'numframes' / Int32sl,
+
     'nummovements' / Int32sl,
-    'movementindex' / Int32sl,
+    'movementindex' / Int32sl,  # mstudiomovement_t
+
     'unused1' / Int32sl[6],
+
     'animblock' / Int32sl,
-    'animindex' / Int32sl,
+    'animindex' / Int32sl,  # mstudioanim_t
+
     'numikrules' / Int32sl,
     'ikruleindex' / Int32sl,
+
     'animblockikruleindex' / Int32sl,
+
     'numlocalhierarchy' / Int32sl,
     'localhierarchyindex' / Int32sl,
+
     'sectionindex' / Int32sl,
     'sectionframes' / Int32sl,
+
     'zeroframespan' / Int16sl,
     'zeroframecount' / Int16sl,
     'zeroframeindex' / Int32sl,
+
+    'name' / Pointer(this.sznameindex + this._.localanimindex + \
+                     (this.i * 96), CString('ascii')),
 )
+
+print(mstudioanimdesc_t.sizeof())
 
 mstudioautolayer_t = Struct(
     'iSequence' / Int16sl,
@@ -347,18 +452,27 @@ studiohdr2_t = Struct(
     'm_nBoneFlexDriverCount' / Int32sl,
     'm_nBoneFlexDriverIndex' / Int32sl,
 )
+
+
+def treat(obj, ctx):
+    print(obj)
+    print(ctx)
+
+
+skinfamily = Array(this._.numskinref, Int16sl)
+skintable = Struct('families' / skinfamily[this._.numskinfamilies])
 studiohdr_t = Struct(
     'id' / Int32sl,
     'version' / Int32sl,
     'checksum' / Int32sl,
     'name' / PaddedString(64, "ascii"),
     'dataLength' / Int32sl,
-    'eyeposition' / Float32l[3],
-    'illumposition' / Float32l[3],
-    'hull_min' / Float32l[3],
-    'hull_max' / Float32l[3],
-    'view_bbmin' / Float32l[3],
-    'view_bbmax' / Float32l[3],
+    'eyeposition' / Vector,
+    'illumposition' / Vector,
+    'hull_min' / Vector,
+    'hull_max' / Vector,
+    'view_bbmin' / Vector,
+    'view_bbmax' / Vector,
     'flags' / Int32sl,
 
     'numbones' / Int32sl,
@@ -367,23 +481,27 @@ studiohdr_t = Struct(
     'RemapSeqBone' / Int32sl,  # todo?
     'RemapAnimBone' / Int32sl,  # todo?
 
+    'numhitboxsets' / Int32sl,  # !ok
+    'hitboxsetindex' / Int32sl,  # !ok
+
     'numbonecontrollers' / Int32sl,  # doesn't look right
     'bonecontrollerindex' / Int32sl,  # doesn't look right
-    'numhitboxsets' / Int32sl,
-    'hitboxsetindex' / Int32sl,
+
 
     'numlocalanim' / Int32sl,
     'localanimindex' / Int32sl,
+
     'numlocalseq' / Int32sl,
     'localseqindex' / Int32sl,
 
-    'numtextures' / Int32sl,
-    'textureindex' / Int32sl,
-    'numcdtextures' / Int32sl,
-    'cdtextureindex' / Int32sl,
-    'numskinref' / Int32sl,
-    'numskinfamilies' / Int32sl,
-    'skinindex' / Int32sl,
+    'numtextures' / Int32sl,  # !ok
+    'textureindex' / Int32sl,  # !ok
+    'numcdtextures' / Int32sl,  # !ok
+    'cdtextureindex' / Int32sl,  # !ok
+    'numskinref' / Int32sl,  # !ok
+    'numskinfamilies' / Int32sl,  # !ok
+    'skinindex' / Int32sl,  # !ok
+
     'numbodyparts' / Int32sl,
     'bodypartindex' / Int32sl,
     'numlocalattachments' / Int32sl,
@@ -432,17 +550,20 @@ studiohdr_t = Struct(
     'studiohdr2index' / Int32sl,
     'unused2' / Int32sl,
 
-    'studiohdr2_t' / Pointer(this.studiohdr2index, studiohdr2_t),
-    'textures' / Pointer(this.textureindex,
-                         mstudiotexture_t[this.numtextures]),
-    'cdtextures' / Pointer(this.cdtextureindex,
-                           mstudiocdtexture_t[this.numcdtextures]),
-    'bodyparts' / Pointer(this.bodypartindex,
-                          mstudiobodyparts_t[this.numbodyparts]),
-    'bones' / Pointer(this.boneindex, mstudiobone_t[this.numbones]),
 
-    'bonecontrollers' / Pointer(this.bonecontrollerindex,
-                                mstudiobonecontroller_t[this.numbonecontrollers]),
+    # 'studiohdr2_t' / Pointer(this.studiohdr2index, studiohdr2_t),
+    # 'textures' / Pointer(this.textureindex,
+    # mstudiotexture_t[this.numtextures]),
+    # 'cdtextures' / Pointer(this.cdtextureindex,
+    #                      mstudiocdtexture_t[this.numcdtextures]),
+    # 'skintable' / Pointer(this.skinindex, skintable)
+    #'anims' / Pointer(this.localanimindex, mstudioanimdesc_t[this.numlocalanim])
+    # 'bodyparts' / Pointer(this.bodypartindex,
+    #                      mstudiobodyparts_t[this.numbodyparts]),
+    # 'bones' / Pointer(this.boneindex, mstudiobone_t[this.numbones]),
+
+    # 'bonecontrollers' / Pointer(this.bonecontrollerindex,
+    #                            mstudiobonecontroller_t[this.numbonecontrollers]),
     'hitboxsets' / Pointer(this.hitboxsetindex,
                            mstudiohitboxset_t[this.numhitboxsets]),
 
